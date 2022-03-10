e3bb0cda0f7353806569858d9d6e6ebb31931bf5
JAISHRI <sjayashree529@gmail.com>
2022-03-10 15:33:50 +0530

    UC-1 changed

--- src/com/bridgelab/employeewage/EmployeeWageComputation.java
+++ src/com/bridgelab/employeewage/EmployeeWageComputation.java
@@ -4,8 +4,7 @@ import java.util.Random;
 
 public class EmployeeWageComputation {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation");
+	public static int attendanceCheck() {
 		Random rand = new Random();
 		int random = rand.nextInt(2);
 		System.out.println(random);
@@ -14,6 +13,15 @@ public class EmployeeWageComputation {
 			System.out.println("Employee is Present");
 		else
 			System.out.println("Employee is Absent");
+
+		return random;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation");
+		
+		 attendanceCheck();
+		
 	}
 
 }
