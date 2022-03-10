35a94f837e5058de82ff6b37224697dcd18d0b80
JAISHRI <sjayashree529@gmail.com>
2022-03-10 16:24:40 +0530

    Uc-2 changed

--- src/com/bridgelab/employeewage/EmployeeWageComputation.java
+++ src/com/bridgelab/employeewage/EmployeeWageComputation.java
@@ -3,23 +3,32 @@ package com.bridgelab.employeewage;
 import java.util.Random;
 
 public class EmployeeWageComputation {
-	static final Integer EMP_WAGE_PER_HOUR = 20;
-	
+	static final int EMP_WAGE_PER_HOUR = 20;
+	static final int IS_PART_TIME = 1;
+	static final int IS_FULL_TIME = 2;
+	static int fullDayHr = 8;
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation");
+	public static int attendanceCheck() {
 		Random rand = new Random();
 		int random = rand.nextInt(2);
 		System.out.println(random);
 
-		if (random == 1) {
+		if (random == 1)
 			System.out.println("Employee is Present");
-			Integer empHr = 8;
-			Integer empWage = EMP_WAGE_PER_HOUR * empHr;
-		}
-		    
 		else
 			System.out.println("Employee is Absent");
+
+		return random;
+	}
+	
+	public static int getDailyWager() {
+		
+		int Daily_wage = EMP_WAGE_PER_HOUR * fullDayHr; 
+		return Daily_wage;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation");
 	}
 
 }
