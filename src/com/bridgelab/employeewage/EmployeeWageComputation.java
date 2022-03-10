dd1f130aca64047d85935b735706e1bec55a6f61
JAISHRI <sjayashree529@gmail.com>
2022-03-10 11:03:11 +0530

    UC-2

--- src/com/bridgelab/employeewage/EmployeeWageComputation.java
+++ src/com/bridgelab/employeewage/EmployeeWageComputation.java
@@ -3,6 +3,8 @@ package com.bridgelab.employeewage;
 import java.util.Random;
 
 public class EmployeeWageComputation {
+	static final int EMP_WAGE_PER_HOUR = 20;
+	
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
@@ -10,8 +12,12 @@ public class EmployeeWageComputation {
 		int random = rand.nextInt(2);
 		System.out.println(random);
 
-		if (random == 1)
+		if (random == 1) {
 			System.out.println("Employee is Present");
+			int empHr = 8;
+			int empWage = EMP_WAGE_PER_HOUR * empHr;
+		}
+		    
 		else
 			System.out.println("Employee is Absent");
 	}
